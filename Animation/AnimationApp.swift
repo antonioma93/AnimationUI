b692cb458975ea46aa6f72ab2b37d95bf21a6aee
Antonio Massa <antonio.massa@zehus.it>
2021-05-14 11:06:37 +0200

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Massa Antonio on 14/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
